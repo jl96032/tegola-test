6d3f8dd490f44b35b1a359e0f61d39c7b4a0e7d0
jl96032 <jl96032@gmail.com>
2022-02-17 11:31:48 +0100

    first commit, add files

---
+++ app.js
@@ -0,0 +1,12 @@
+var http = require('http'), fs = require('fs');
+
+fs.readFile('./index.html', (err, html) => {
+  if (err) {
+    throw err;
+  } 
+  http.createServer( (req,res) => {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write(html);
+    res.end();
+  }).listen(3000);
+})
